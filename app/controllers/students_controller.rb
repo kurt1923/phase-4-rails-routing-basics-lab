ee6a7f2c434472ce64ccb96d90dd4f27dd0369bb
Kurt Vermillion <kurtv0727@gmail.com>
2023-02-01 17:09:44 -0500

    rails

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,18 @@
+class StudentsController < ActionController
+
+    def index 
+        students = Student.all 
+        render json: students 
+    end
+
+    def grades
+        students = Student.order(grade: :desc)
+        render json: students
+    end
+
+    def highest_grade
+        student = Student.order(grade: :desc).first
+        render json: student
+    end
+
+end
